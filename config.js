b996c7b793f63f85d48c2789bacc18b01e15d969
Dela10026 <pvtdailiya100@gmail.com>
2024-06-11 21:52:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -23,7 +23,7 @@
 
 module.exports = {
   TOKEN: "",
-  ownerID: ["1004206704994566164", ""], 
+  ownerID: ["1089605499424604191", ""], 
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "00fbff",
